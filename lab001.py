39de09718f0c03b5ac7b3e5396d8d7d3315af5a0
Sekhar <nagashekar7@gmail.com>
2025-04-25 00:37:16 +0530

    lab001

---
+++ lab001.py
@@ -0,0 +1,7 @@
+score = int(input("enter marks:"))
+if score >=90 and score <=100:
+    print ("you grade is : A")
+elif score >=80 and score <=89:
+    print("your grade is : B")
+else :
+    print("your grade is : F")
---
+++ testing.py
@@ -0,0 +1,97 @@
+# a = 20
+# print(type(a))
+# if a == 10:
+#     print("Good")
+# else:
+#     print("bad")
+
+#============================================
+# score = int(input("enter marks:"))
+# if score >=90 and score <=100:
+#     print ("you grade is : A")
+# elif score >=80 and score <=89:
+#     print("your grade is : B")
+# else :
+#     print("your grade is : F")
+#===========================================
+# Number = int(input("enter number"))
+# if Number %2==0:
+#     print("Give number is even")
+# else:
+#     print("given number is Odd")
+
+#=========================================
+# pass or fail
+
+# a = int(input("enter marks :"))
+# if a>=50:
+#     print ("pass")
+# else:
+#     print("fail")
+#=================================================
+#Age categery
+
+# age = int(input("enter your age : "))
+# if age < 13:
+#     print("you are child")
+# elif age >=13 and age <=19:
+#     print("you are Teenager")
+# else:
+#     print("you are Adult")
+#==================================================
+# year = int(input("Enter year : "))
+# if  year %4==0 or year %400==0:
+#     print("Given year is leap year")
+# else:
+#     print("Not a leap year")
+#========================================================
+# year = int(input("Enter the year:"))
+# if year %100 ==0:
+#     if year % 400 ==0:
+#         print("Leap year")
+#     else:
+#         print("Not a leap year")
+# elif year % 4 ==0:
+#     print("Leap year")
+# else:
+#     print("Not a leap year")
+#=================================================
+# for i in range(10,5,-2):
+#     print(i,end= " ")
+#===========================
+# i=0
+# while i <10:
+#     print(i)
+#     i=i+1
+#
+#==========================
+
+# for i in range(1,10):
+#     print(i)
+#     if i ==6:
+#         break
+#
+#==========================
+
+# for i in range(10):
+#     if i==6 or i==5:
+#         print(i)
+#     else:
+#         pass
+#==========================
+
+# for i in range (10):
+#     if i % 2 == 0:
+#         continue
+#     print(i)
+for i in range(20):
+        print(i)
+
+
+for i in range(20):
+    if i%2==0:
+        print(i)
+
+
+
+
