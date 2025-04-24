2b3d661f301708f234dc9cbed825219977b9dc7f
Sekhar <nagashekar7@gmail.com>
2025-04-25 01:05:30 +0530

    25th April

--- 25April.py
+++ 25April.py
@@ -1,6 +1,15 @@
 #Take a number as input and check whether it is even or odd.
-number = int(input("ENter number : "))
-if number %2 ==0:
-    print("Give number is even")
+# number = int(input("ENter number : "))
+# if number %2 ==0:
+#     print("Give number is even")
+# else:
+#     print("Given number is ODD")
+
+#Input a person's age and check if they are eligible to vote (18+).
+
+Age = int(input("Enter age : "))
+if Age >=18:
+    print("he can eligible for voting")
 else:
-    print("Given number is ODD")
+    print("He can't eligible for voting")
+
