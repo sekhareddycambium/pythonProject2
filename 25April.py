fcdff5da3520d7b20a301a27350d1ccfbb6eed05
Sekhar <nagashekar7@gmail.com>
2025-04-25 00:59:31 +0530

    25th April

---
+++ 25April.py
@@ -0,0 +1,6 @@
+#Take a number as input and check whether it is even or odd.
+number = int(input("ENter number : "))
+if number %2 ==0:
+    print("Give number is even")
+else:
+    print("Given number is ODD")
