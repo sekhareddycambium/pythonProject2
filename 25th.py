eaae250dacb5c2ad6a98a1299441951c4207cdd8
Sekhar <nagashekar7@gmail.com>
2025-04-25 01:30:09 +0530

    25th April

--- 25th.py
+++ 25th.py
@@ -1,11 +1,24 @@
-Sekhar = int(input("ENter your score"))
-if  Sekhar>=90:
-    print("your score is >90 and grade is A")
-elif 80 <= Sekhar <= 89:
-    print("your grade is B")
-elif 70<= Sekhar <=79:
-    print("your grade is C")
-elif 60<= Sekhar <=69:
-    print("your grade is D")
+# Sekhar = int(input("ENter your score"))
+# if  Sekhar>=90:
+#     print("your score is >90 and grade is A")
+# elif 80 <= Sekhar <= 89:
+#     print("your grade is B")
+# elif 70<= Sekhar <=79:
+#     print("your grade is C")
+# elif 60<= Sekhar <=69:
+#     print("your grade is D")
+# else:
+#     print("""your grade is F  "you are FAIL" """)
+
+#Check whether a given year is a leap year or not.
+
+year =int(input("Enter year : "))
+if year % 100 == 0 :
+    if year % 400 == 0:
+        print("Give year is leap year")
+    else:
+        print("Given year is Not leap year")
+elif year % 4 ==0:
+    print("it's leap year")
 else:
-    print("""your grade is F  "you are FAIL" """)
+    print("It's not leap year")
