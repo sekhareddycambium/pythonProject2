cdf7c7dd464cacc331f1faf089e95d628cdb6d24
Sekhar <nagashekar7@gmail.com>
2025-04-25 00:55:35 +0530

    lab001

--- lab001.py
+++ lab001.py
@@ -1,7 +1,17 @@
-score = int(input("enter marks:"))
-if score >=90 and score <=100:
-    print ("you grade is : A")
-elif score >=80 and score <=89:
-    print("your grade is : B")
-else :
-    print("your grade is : F")
+# score = int(input("enter marks:"))
+# if score >=90 and score <=100:
+#     print ("you grade is : A")
+# elif score >=80 and score <=89:
+#     print("your grade is : B")
+# else :
+#     print("your grade is : F")
+
+
+#Write a program to check whether a number is positive, negative, or zero.
+Number = int(input("Enter number"))
+if Number > 0:
+    print("Give number is positive")
+elif Number < 0:
+    print("Given number is negative")
+else:
+    print ("the number is zero")
